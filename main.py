688db09fe269aa7d7795f48ee8b961590952e68f
d3ng03 <d3ng03@gmail.com>
2023-10-26 03:19:40 +0900

    get dat by remote

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import eyetracking
 import emotion
 import cv2
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from urllib import request
+
+
+path = "~/Desktop/AI-concentrate_graph/"
+dat = "shape_predictor_68_face_landmarks.dat"
+remote_dat = "https://github.com/italojs/facial-landmarks-recognition/blob/master/shape_predictor_68_face_landmarks.dat"
+
+def check_dat():
+    if(os.path.exists(path + dat)):
+        return
+    else:
+        request.urlretrieve(remote_dat,(path+dat))
+        return
 
 
 
 if __name__ == "__main__":
+    check_dat()
     eyetracking_score = eyetracking.calculate_eyetracking()
     emotion_score = emotion.calculate_emotion()
